8fd51fb5c02407a71c14704de2d2a727640eebbc
Documenter.jl <documenter@juliadocs.github.io>
2022-01-16 02:14:05 +0000

    build based on c2cb60a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumMaterials","category":"page"},{"location":"#QuantumMaterials","page":"Home","title":"QuantumMaterials","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumMaterials.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumMaterials]","category":"page"}]
+}
